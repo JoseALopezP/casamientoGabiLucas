322daafb236a520a1923c2edaaa7797d8f0d7e5f
JoseALopezP <jalp42@live.com>
2022-10-14 12:12:50 -0300

    Added structure for html in introduction salutations

---
+++ src/components/Introduction/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Introduction = () => {
+    return (
+        <>
+            <h4>09 | 12 | 22</h4>
+            <div className='introductionNames'>
+                <h1>Gabi</h1>
+                <h1>&</h1>
+                <h1>Lucas</h1>
+            </div>
+            <h4><span>"Uno solo puede ser vencido pero dos pueden resistir. ¡la cuerda de tres hilos no se rompe fácilmente!"</span> Eclesiastes 4, 12</h4>
+        </>
+    );
+}
+
+export default Introduction;
