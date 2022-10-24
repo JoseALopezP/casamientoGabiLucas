c40088f60a6ab95b3fcdef1d314b87449daaaa85
JoseALopezP <jalp42@live.com>
2022-10-24 11:22:26 -0300

    updating

--- src/components/Context/ListContext.js
+++ src/components/Context/ListContext.js
@@ -1,7 +1,5 @@
-import { getDocs } from "firebase/firestore";
-import React, { createContext, useState, useEffect } from "react";
-import db from '../services/firebase';
-import { doc, deleteDoc, addDoc, collection } from "firebase/firestore";
+import { createContext, useState } from "react";
+
 export const ListContext = createContext({});
 
 const {Provider} = ListContext;
@@ -9,41 +7,9 @@ const {Provider} = ListContext;
 export const ListProvider = ({defaultValue = [], children}) => {
     const [list, setList] = useState(defaultValue);
     
-    const clearList = () => {
-        setList([]);
-    }
-    const updateList = async() => {
-        try{
-            const document = collection(db, 'Products')
-            const col = await getDocs(document)
-            console.log('col.docs', col.docs)
-            const result = col.docs.map((doc) => doc = {id: doc.id, ...doc.data()})
-            setList(result)
-        }catch(error){
-            console.log(error)
-        }
-    } 
-    useEffect(() =>{
-        updateList()
-    }, [])
-
-    const addGuest = async (data) => {
-        
-    }
-
-    const removeGuest = async (id) => {
-        try{
-            await deleteDoc(doc(db, 'confirmationList', id))
-        }catch (error){
-            console.log(error)
-        }
-    }
 
     const context = {
-        clearList,
-        updateList,
-        removeGuest,
-        list
+
     }
     return(
         <>
