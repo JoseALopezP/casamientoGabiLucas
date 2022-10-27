57b612869fd67cfc0bec4c46a2cac66c86268afd
JoseALopezP <jalp42@live.com>
2022-10-27 10:31:01 -0300

    Footer styled

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footerBlock'>
-            <h5> Por favor, recordá confirmar tu asistencia antes del 26 de noviembre</h5>
-            <p>¡Te esperamos para compartir este momento tan importante de nuestras vidas! </p>
-            <p>Gabi & Lucas</p>
+            <div className="footerMsg">
+                <h5> Por favor, recordá confirmar tu asistencia antes del 26 de noviembre</h5>
+                <p>¡Te esperamos para compartir este momento tan importante de nuestras vidas! </p>
+                <p>Gabi & Lucas</p>
+            </div>
             <div className="contactInfo">
                 <p className='contactMsg'>Por cualquier duda o modificación, podés contactarte con cualquiera de nosotros a nuestros números de celular:</p>
                 <p className='contactGabi'>264-45475366 (Gabi)</p>
